9f7cc2fe1ab60468ed27de99ce7c2316ef48fbd2
Tamer Abbass <tamerh.abbass@gmail.com>
2020-07-19 14:58:02 -0700

    Fixed Authentication

--- trading_clock.py
+++ trading_clock.py
@@ -1,9 +1,12 @@
 import alpaca_trade_api as tradeapi
+import trading_config as tc
 
 if __name__ == "__main__":
 
     api = tradeapi.REST(
-
+        tc.authentication["APCA-API-SECRET-ID"],
+        tc.authentication["APCA-API-SECRET-KEY"],
+        tc.authentication["API-URL"]
     )
 
 clock = api.get_clock()
