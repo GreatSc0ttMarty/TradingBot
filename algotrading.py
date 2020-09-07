3165de0c17cf252057384c7e707ce3bd1a252d86
Tamer Abbass <tamerh.abbass@gmail.com>
2020-09-06 18:45:50 -0700

    New script from algo trading course.

---
+++ algotrading.py
@@ -0,0 +1,184 @@
+import bs4 as bs
+import requests
+import pickle
+import datetime as dt
+import os
+import pandas as pd
+import pandas_datareader. data as web
+import time
+import matplotlib.pyplot as plt
+from matplotlib import style
+import numpy as np
+from collections import Counter
+
+style.use('dark_background')
+
+def save_sp500_tickers():
+    resp = requests.get('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
+    soup = bs.BeautifulSoup(resp.text, 'lxml')
+    table = soup.find('table', {'class':'wikitable sortable'})
+    tickers = []
+    for row in table.findAll('tr')[1:]:
+        ticker = row.findAll('td')[0].text
+        tickers.append(ticker.rstrip())
+
+    with open("sp500tickers.pickle", "wb") as f:
+        pickle.dump(tickers, f)
+    
+    print(tickers)
+    return tickers
+
+#save_sp500_tickers()
+
+
+def get_data_from_yahoo(reload_sp500=False):
+    if reload_sp500:
+        tickers = save_sp500_tickers()
+
+    else:
+        with open("sp500tickers.pickle", "rb") as f:
+            tickers = pickle.load(f)
+
+    if not os.path.exists('stock_dfs'):
+        os.makedirs('stock_dfs')
+
+    start = dt.datetime(2015, 1, 1)
+    end = dt.datetime(2020, 8, 5)
+
+    for ticker in tickers:
+        if not os.path.exists('stock_dfs/{ticker}.csv'):
+            if '.' in ticker:
+                ticker = ticker.replace('.', '-')
+            time.sleep(1)
+            print(ticker)
+            df = web.DataReader(ticker, 'yahoo', start, end)
+            df.to_csv('stock_dfs/{}.csv'.format(ticker))
+        else:
+            print(f'Already have {ticker}')
+
+#get_data_from_yahoo()
+
+def compile_data():
+    with open("sp500tickers.pickle","rb") as f:
+        tickers = pickle.load(f)
+
+    main_df = pd.DataFrame()
+
+    for count,ticker in enumerate(tickers):
+        if '.' in ticker:
+            ticker = ticker.replace('.', '-')
+        df = pd.read_csv(f'stock_dfs/{ticker}.csv')
+        df.set_index('Date', inplace=True)
+
+        df.rename(columns={'Adj Close':ticker}, inplace=True)
+
+        df.drop(['High','Low','Open','Close','Volume'],1,inplace=True)
+        #Date,High,Low,Open,Close,Volume,Adj Close
+        
+        if main_df.empty:
+            main_df = df
+        else:
+            main_df = main_df.join(df, how='outer')
+
+        if count % 10 == 0:
+            print(count)
+    print(main_df.head())
+    main_df.to_csv('sp500_joined_closes.csv')
+
+#compile_data()
+
+
+def visualize_data():
+    df = pd.read_csv('sp500_joined_closes.csv')
+    #df['AAPL'].plot()
+    #plt.show()
+    df_corr = df.corr()
+    print(df_corr.head())
+
+    data = df_corr.values
+    fig = plt.figure()
+    ax = fig.add_subplot(1,1,1)
+
+    heatmap = ax.pcolor(data, cmap=plt.cm.RdYlGn)
+    fig.colorbar(heatmap)
+    ax.set_xticks(np.arange(data.shape[0]) + 0.5, minor=False)
+    ax.set_yticks(np.arange(data.shape[1]) + 0.5, minor=False)
+
+    ax.invert_yaxis()
+    ax.xaxis.tick_top()
+
+    column_labels = df_corr.columns
+    row_labels = df_corr.index
+
+    ax.set_xticklabels(column_labels)
+    ax.set_yticklabels(row_labels)
+    plt.xticks(rotation=90)
+    heatmap.set_clim(-1, 1)
+
+    plt.tight_layout()
+    plt.show()
+
+#visualize_data()
+
+
+
+# Machine Learning
+def process_data_for_labels(ticker):
+    hm_days = 21
+    df = pd.read_csv('sp500_joined_closes.csv', index_col=0)
+    tickers = df.columns.values.tolist()
+    df.fillna(0,inplace=True)
+    print(df.head())
+    # percentage changed
+    for i in range(1, hm_days+1):
+        df[f'{ticker}_{i}d'] = (df[ticker].shift(-i) - df[ticker]) / df[ticker]
+    df.fillna(0, inplace=True)
+    return tickers, df
+
+#process_data_for_labels('XOM')
+
+
+def buy_sell_hold(*args):
+    # Buy: 1, Sell: -1, Hold: 0
+    cols = [c for c in args]
+    requirement = 0.2
+    for col in cols:
+        if col > requirement:
+            return 1
+        if col < -requirement:
+            return -1
+    return 0
+
+#buy_sell_hold('XOM')
+
+
+def extract_featuresets(ticker):
+    tickers, df = process_data_for_labels(ticker)
+    #df[f'{ticker}_target'] = list(map(buy_sell_hold, [df[f'{ticker}_{i}d'] for i in range(1, 8)]))
+    df['{}_target'.format(ticker)] = list(map(buy_sell_hold,
+                                               df['{}_1d'.format(ticker)],
+                                               df['{}_2d'.format(ticker)],
+                                               df['{}_3d'.format(ticker)],
+                                               df['{}_4d'.format(ticker)],
+                                               df['{}_5d'.format(ticker)],
+                                               df['{}_6d'.format(ticker)],
+                                               df['{}_7d'.format(ticker)]))
+    vals = df[f'{ticker}_target'].values
+    str_vals = [str(i) for i in vals]
+    print('Data spread: ', Counter(str_vals))
+
+    df.fillna(0, inplace=True)
+    df = df.replace([np.inf, -np.inf], np.nan)
+    df.dropna(inplace=True)
+
+    df_vals = df[[ticker for ticker in tickers]].pct_change()
+    df_vals = df_vals.replace([np.inf, -np.inf], 0)
+    df_vals.fillna(0, inplace=True)
+
+    X = df_vals.values
+    y = df[f'{ticker}_target'].values
+
+    return X,y,df
+
+extract_featuresets('AAPL')
+
